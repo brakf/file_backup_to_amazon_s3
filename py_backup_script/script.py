af235f2c83888e761f657132fb84ad8d9f156dde
Fabian Brakowski <fabian@brakowski.cloud>
2025-04-03 01:22:16 +0200

    Add BucketFolder as config parameter

--- py_backup_script/script.py
+++ py_backup_script/script.py
@@ -1,5 +1,3 @@
-
-# Get config
 from datetime import datetime
 import logging
 from logging.handlers import RotatingFileHandler
@@ -9,67 +7,61 @@ import zipfile
 import yaml
 import boto3
 import time
+
 start_time = time.time()
 if len(sys.argv) == 2:
     config_path = sys.argv[1]
 else:
-    config_path = os.path.join(os.path.dirname(__file__),"config.yaml")
+    config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
     
 config = yaml.load(open(config_path), Loader=yaml.FullLoader)
 
-
-logging.basicConfig(encoding='utf-8', level=logging.INFO, handlers=[RotatingFileHandler(os.path.join(os.path.dirname(__file__),"s3_upload.log"), maxBytes=1024000, backupCount=1)])
-
+logging.basicConfig(encoding='utf-8', level=logging.INFO, handlers=[RotatingFileHandler(os.path.join(os.path.dirname(__file__), "s3_upload.log"), maxBytes=1024000, backupCount=1)])
 
 logging.info(["---------NEW EXECUTION STARTED---------", datetime.now().__str__()])
 
+s3_client = boto3.client('s3', aws_access_key_id=config.get("AWSAccessKey"), aws_secret_access_key=config.get("AWSSecretKey"))
 
+bucket_folder = config.get("BucketFolder", "")
+if bucket_folder and not bucket_folder.endswith("/"):
+    bucket_folder += "/"
 
-s3_client = client = boto3.client('s3', aws_access_key_id=config.get("AWSAccessKey"), aws_secret_access_key=config.get("AWSSecretKey"))
-
-for file in config.get("FilesToBackup"):
-
+for file in config.get("FilesToBackup", []):
     try: 
-
-        
-
-        path = config.get("BasePath")+file
-        filename_zip = file+".zip"
-        path_zip = os.path.join(os.path.dirname(__file__),filename_zip)  
+        path = os.path.join(config.get("BasePath", ""), file)
+        filename_zip = file + ".zip"
+        path_zip = os.path.join(os.path.dirname(__file__), filename_zip)  
         zip_time = time.time()
 
-        #test if file exists
+        # Test if file exists
         if not os.path.exists(path):
-           raise Exception("file not found: "+path) 
-		
-        logging.info("Zipping file: "+path)
-		
-		
-		
-        with zipfile.ZipFile(path_zip, mode='w', compression=zipfile.ZIP_DEFLATED, allowZip64=True, compresslevel=6) as zip:
-            zip.write(path)
+            raise Exception("File not found: " + path) 
+        
+        logging.info("Zipping file: " + path)
+        
+        with zipfile.ZipFile(path_zip, mode='w', compression=zipfile.ZIP_DEFLATED, allowZip64=True, compresslevel=6) as zipf:
+            zipf.write(path, arcname=file)
+        
         logging.debug("---Zip Time %s seconds ---" % (time.time() - zip_time))
-        logging.info("Uploading file: "+path_zip)
+        logging.info("Uploading file: " + path_zip)
+        
         try:
             upload_time = time.time()
-            response = s3_client.upload_file(path_zip, config.get("BackupBucket"), filename_zip)
+            s3_key = bucket_folder + filename_zip
+            s3_client.upload_file(path_zip, config.get("BackupBucket"), s3_key)
             logging.debug("---Upload Time %s seconds ---" % (time.time() - upload_time))
         except boto3.ClientError as e:
             logging.error(e)
             raise Exception(e)
 
-        logging.info("Deleting file: "+path_zip)
+        logging.info("Deleting file: " + path_zip)
         os.remove(path_zip)
 
     except Exception as e:
         logging.error(e)
-
         if os.path.exists(path_zip):
-            logging.info("Deleting file as cleanup: "+path_zip)
+            logging.info("Deleting file as cleanup: " + path_zip)
             os.remove(path_zip) 
-		
-		
+        
 logging.debug("---Total Time %s seconds ---" % (time.time() - start_time))
 logging.info(["--------- EXECUTION ENDED---------", datetime.now().__str__()])
-
-
